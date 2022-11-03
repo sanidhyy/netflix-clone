13a7006253fa850d1a2bec977628afa9cdccb08b
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-11-03 20:50:16 +0530

    fixed typo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -42,7 +42,9 @@ const Banner = () => {
           <button className="banner__button">My List</button>
         </div>
 
-        <p className="banner__description">{truncate(movie?.overview, 150)}</p>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
 
       <div className="banner--fadeBottom"></div>
