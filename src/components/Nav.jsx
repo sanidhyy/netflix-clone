e5ebaca967b04acff6c0339d6de896cbf13639e3
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-11-04 17:11:19 +0530

    nav completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import logo from "../assets/logo.png";
 import avatar from "../assets/avatar.png";
 
+import "./Nav.css";
+
 const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img src={logo} alt="Netflix" className="nav__logo" />
 
       <img src={avatar} alt="Avatar" className="nav__avatar" />
